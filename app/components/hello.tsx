10c7d2bd232c6faf1538fb3f168393623d3dc623
sivaprakasam-07 <tamilmanisivaprakasam5@gmail.com>
2025-03-10 07:04:48 +0530

    learned server side rendering and configured tailwind css

---
+++ app/components/hello.tsx
@@ -0,0 +1,8 @@
+"use client"
+function Hello(){
+    console.log("I am a Client Component");
+    return(
+        <h1 className="text-3l text-blue-700">Hello World</h1>
+    );
+}
+export default Hello;
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,10 @@
+import Hello from '../app/components/hello'
 export default function Page() {
-  console.log("What am I doing here? -- server/client");
+  console.log("What am I doing here? -- server");
   return(
-    <h1 className="text-2xl">Sivaprakasam</h1>
+    <>
+      <h1 className="font-bold text-3xl text-white-700">Hello World</h1>
+      <Hello />
+    </>
   )
 }
